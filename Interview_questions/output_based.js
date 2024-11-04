8c021752bb63e688b897f13569b3eed66033b723
Tamal-buro <56538942+Tamal-buro@users.noreply.github.com>
2024-11-05 00:42:25 +0530

    Output_based_questions

---
+++ Interview_questions/output_based.js
@@ -0,0 +1,30 @@
+// Output for below - 
+
+it("test one", function() {
+    console.log('John')
+    const lastName = cy.wrap('Peter');
+    console.log(lastName);
+})
+/* 
+    John
+    Peter
+*/
+
+
+
+it("test two”, function() {
+    console.log(“John”);
+    cy.wrap(‘Peter’).then(name => {
+        console.log(name);
+    })
+    console.log(‘Automation’);
+})
+
+/* 
+   John
+   Automation
+   Peter
+
+
+*/
+
